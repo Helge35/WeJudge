dfc4e53f80dc9cdb4da7b25b1ba88d0dc4efcfd6
Oleg <olegbro@gmail.com>
2018-07-13 15:09:30 +0300

    Create Host

---
+++ WeJudgeHost/WeJudgeHost/BL/Sevices/Bootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WeJudgeHost.BL.Sevices
+{
+    public static class Bootstrapper
+    {
+        internal static void Register(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+    }
+}
---
+++ WeJudgeHost/WeJudgeHost/BL/Sevices/EntitiesMapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeJudgeHost.BL.Sevices
+{
+    public static class EntitiesMapper
+    {
+        internal static void Map()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
